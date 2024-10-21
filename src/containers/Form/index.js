d3890bf1fbf743a6b4e5d7ebc5b4bc7a5fc96789
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-10-21 10:42:39 +0200

    Ajout d'un message de confirmation d'envoi du formulaire de contact

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -8,6 +8,8 @@ const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500)
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+
+  
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
@@ -16,6 +18,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -23,6 +26,7 @@ const Form = ({ onSuccess, onError }) => {
     },
     [onSuccess, onError]
   );
+
   return (
     <form onSubmit={sendContact}>
       <div className="row">
