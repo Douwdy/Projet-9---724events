8ed54b5a36058f5ec351102097ed18b75717d8f8
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-10-23 09:39:04 +0200

    Création des test unitaires pour les Icones du footer et du systeme de datation

--- src/components/Icon/index.test.js
+++ src/components/Icon/index.test.js
@@ -11,8 +11,26 @@ describe("Icon component", () => {
     });
     describe("When a icon is created with name facebook", () => {
         it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
-            // to complete
+            render(<Icon name="facebook" />)
+            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('bbea4c9e40773b969fdb6e406059f853')
         });
     });
-})
-
+    describe("When a icon is created with name twitter", () => {
+        it("the icon contain this path hash value 8e7d2c8e8e8e8e8e8e8e8e8e8e8e8e8e", () => {
+            render(<Icon name="twitter" />)
+            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('82f5be4a5c07199cb75dacec50b90b2a')
+        });
+    });
+    describe("When a icon is created with name youtube", () => {
+        it("the icon contain this path hash value 9f8e7d6c5b4a3a2b1c0d9e8f7d6c5b4a", () => {
+            render(<Icon name="youtube" />)
+            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('43342876c2fc40e5b2afe621443ff95b')
+        });
+    });
+    describe("When a icon is created with name close", () => {
+        it("the icon contain this path hash value 0a1b2c3d4e5f6g7h8i9j0a1b2c3d4e5f", () => {
+            render(<Icon name="close" />)
+            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('fe53fa5bf815b6d5983fcadf9a15d3d1')
+        });
+    });
+});
--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,24 @@
-/**
- * 
- */
+import { getMonth, MONTHS } from './index';
 
-describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+describe('getMonth', () => {
+    const testCases = [
+        { date: new Date(2023, 0, 1), expected: MONTHS[1] },
+        { date: new Date(2023, 1, 1), expected: MONTHS[2] },
+        { date: new Date(2023, 2, 1), expected: MONTHS[3] },
+        { date: new Date(2023, 3, 1), expected: MONTHS[4] },
+        { date: new Date(2023, 4, 1), expected: MONTHS[5] },
+        { date: new Date(2023, 5, 1), expected: MONTHS[6] },
+        { date: new Date(2023, 6, 1), expected: MONTHS[7] },
+        { date: new Date(2023, 7, 1), expected: MONTHS[8] },
+        { date: new Date(2023, 8, 1), expected: MONTHS[9] },
+        { date: new Date(2023, 9, 1), expected: MONTHS[10] },
+        { date: new Date(2023, 10, 1), expected: MONTHS[11] },
+        { date: new Date(2023, 11, 1), expected: MONTHS[12] },
+    ];
+
+    testCases.forEach(({ date, expected }, index) => {
+        it(`should return the correct month name for ${MONTHS[index + 1]}`, () => {
+            expect(getMonth(date)).toBe(expected);
         });
     });
-})
-
+});
